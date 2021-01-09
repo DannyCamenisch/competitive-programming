94be228bcaa4bbe2fea900ca58bbb10df3153735
Danny Camenisch <9078410+DannyCamenisch@users.noreply.github.com>
2021-01-09 15:56:59 +0100

    Create ADifferentProblem.java

---
+++ kattis/ADifferentProblem.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class ADifferentProblem {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        ADifferentProblem m = new ADifferentProblem();
+        
+        while(in.hasNext) {
+            m.run;
+        }
+        
+        in.close();
+    }
+    
+    private void run(Scanner in) {
+      System.out.println(Math.abs(in.nextLong() - in.nextLong()));
+    }
+}
