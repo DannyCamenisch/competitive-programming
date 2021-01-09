eec3137404b0dd7fa1a4720088e31a9ff7d9ec22
Danny Camenisch <9078410+DannyCamenisch@users.noreply.github.com>
2021-01-09 16:00:10 +0100

    Create R2.java

---
+++ kattis/R2.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class R2 {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        R2 m = new R2();
+        
+        while(in.hasNext()) {
+          m.run();
+        }
+        
+        in.close();
+     }
+        
+     private void run(Scanner in) {
+        int r1 = in.nextInt();
+        int s = in.nextInt();
+        
+        int r2 = 2*s-r1;
+        
+        System.out.println(r2);
+    }
+}
