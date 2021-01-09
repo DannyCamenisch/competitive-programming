02b2442ef8a389504f7c5329f0a70ec5349dd888
Danny Camenisch <9078410+DannyCamenisch@users.noreply.github.com>
2021-01-09 15:50:05 +0100

    Create TakeTwoStones.java

---
+++ kattis/TakeTwoStones.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class TakeTwoStones {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        TakeTwoStones m = new TakeTwoStones();
+        
+        while(in.hasNext()) {
+          m.run();
+        }
+        
+        in.close();
+    }
+    
+    private void run(Scanner in) {
+      System.out.println(in.nextInt() % 2 == 1 ? "Alice" : "Bob");
+    }
+}
