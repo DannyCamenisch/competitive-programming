ca003d3fc84dc4b890b110e4338480b034d90048
Danny Camenisch <9078410+DannyCamenisch@users.noreply.github.com>
2021-01-09 15:51:19 +0100

    Create Main.java

---
+++ templates/Main.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        Main m = new Main();
+        
+        while(in.hasNext()) {
+          m.run();
+        }
+        
+        in.close();
+    }
+    
+    private void run(Scanner in) {
+      // TODO: insert code
+    }
+}
